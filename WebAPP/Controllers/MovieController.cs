eb5de862a2e8d110a1c27a53c9cbd2e7e9dac8ab
Ladda <vladda@telstrasuper.local>
2019-12-06 16:55:22 +1100

    Basic model, controller, route added

---
+++ WebAPP/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPP.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public string Index()
+        {
+            return "This is Index action method of MovieController";
+        }
+    }
+}
